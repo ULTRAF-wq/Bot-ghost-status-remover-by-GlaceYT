72f09709f4873f2413a28fb210dcb1f1bd4629ec
ULTRAF-wq <fron16459@gmail.com>
2024-11-09 18:12:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["𝗕𝗼𝘁 𝗠𝗮𝗱𝗲 𝗕𝘆 𝗦𝗛𝗤𝗟7"];
+const statusMessages = ["𝗕𝗼𝘁 𝗠𝗮𝗱𝗲 𝗕𝘆 𝗕𝗔𝗗𝗪𝗜"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
